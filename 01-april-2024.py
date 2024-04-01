89a20d9d827f88d4dba8404001c3182e406a4ff4
Ankur Tiwari <161157271+ankurtiwari2026@users.noreply.github.com>
2024-04-01 23:29:42 +0530

    Create 01-april-2024.py

---
+++ 01-april-2024.py
@@ -0,0 +1,10 @@
+58. Length of Last Word:
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.strip().split()
+        
+        if not words:
+            return 0
+        
+        return len(words[-1])
+        
